fcd990a45a946fb8f58133f2ee44aefd86ff079f
DoTienOanh <164726088+DoTienOanh@users.noreply.github.com>
2024-03-25 14:24:49 +0700

    test_model.py

--- test_model.py
+++ test_model.py
@@ -1,4 +1,6 @@
 import pickle as pickle
 import pandas as pd
 model = pickle.load(open("bank_churn_prediction.pkl","rb"))
-new_customer = [[
+new_customer = [[0.472, 0.067568, 0.2, 0.000000, 0.333333, 0.0, 1.0, 0.804903, 1.0]]
+result = model.predict(new_customer)
+print(result)
